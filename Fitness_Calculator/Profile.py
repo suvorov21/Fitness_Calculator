c55fa3404996a35a5e8a46fd3c7ad1a0508fb49b
AlexBesouro <zhukovoleksandrv@gmail.com>
2024-03-30 19:35:03 +0100

    Class to represent a user

---
+++ Fitness_Calculator/Profile.py
@@ -0,0 +1,29 @@
+"""
+Class to represent a user
+"""
+class User:
+    def __init__(self, first, last, sex, age, weight, height):
+        self.first = first
+        self.last = last
+        self.age = age
+        self.sex = sex
+        self.weight = weight
+        self.height = height
+
+    def __repr__(self):
+        return f"It is {self.first}"
+
+    def set_goal(self,  weight_goal):
+        self.weight_goal = weight_goal
+
+    def days_to_goal(self, days):
+        self.days = days
+
+    def level(self, level_of_activity):
+        self.level_of_activity = level_of_activity
+
+alex = User('Alex', 'Bes', 'men', 28, 85, 180)
+print(alex)
+print(alex.weight)
+alex.weight_goal = 60
+print(alex.weight_goal)
